76ed21a0f59636a0e64d252f3d5067f0f11c61fa
UndyingOP <alexanderrainsnetufo@gmail.com>
2026-03-12 04:10:21 +0100

    Add function to change text content of title

---
+++ script.js
@@ -0,0 +1,3 @@
+function changetext(){
+  document.getElementById("title").innerHTML = " Dear Essie, I miss you and I can't stop thinking about you. Your vampire teeth, colesterol in right places, and your curious mind. And when you said you loved me, it felt nice. Your ex boyfriend Dims ";
+}
